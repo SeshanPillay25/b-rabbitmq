8b152fcc3e58c7505445ec71a605f1fcd29bd625
Seshan Pillay <seshanpillay25@gmail.com>
2020-06-13 16:39:35 +0200

    Configured dependency container

---
+++ BRabbitMQ/BRabbitMQ.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using BRabbitMQ.Domain.Core.Bus;
+using BRabbitMQ.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BRabbitMQ.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
