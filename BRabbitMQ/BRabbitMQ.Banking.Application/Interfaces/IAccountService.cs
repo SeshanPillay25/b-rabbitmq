444cff1c19add486688f2f74fa184433dd491984
Seshan Pillay <seshanpillay25@gmail.com>
2020-06-14 14:12:53 +0200

    Implemented banking model, interfaces, and dbcontext

---
+++ BRabbitMQ/BRabbitMQ.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BRabbitMQ.Banking.Domain.Models;
+
+namespace BRabbitMQ.Banking.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
---
+++ BRabbitMQ/BRabbitMQ.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BRabbitMQ.Banking.Application.Interfaces;
+using BRabbitMQ.Banking.Domain.Interfaces;
+using BRabbitMQ.Banking.Domain.Models;
+
+namespace BRabbitMQ.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        //Injecting repository
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepository.GetAccounts();
+        }
+    }
+}
