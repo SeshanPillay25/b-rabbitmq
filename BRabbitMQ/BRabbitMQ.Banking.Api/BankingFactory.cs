34970858f892c79e04a278760adbd3f5709c6120
Seshan Pillay <seshanpillay25@gmail.com>
2020-06-14 23:15:30 +0200

    Configured banking api AppSettings, Startup, Swagger, and Mediatr

---
+++ BRabbitMQ/BRabbitMQ.Banking.Api/BankingFactory.cs
@@ -0,0 +1,17 @@
+﻿using BRabbitMQ.Banking.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BRabbitMQ.Banking.API
+{
+    public class BankingFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+        public BankingDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BankingDbContext>();
+            optionsBuilder.UseSqlServer("Server=GHOSTPC\\SQLEXPRESS;Database=BankingDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new BankingDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- BRabbitMQ/BRabbitMQ.Banking.Api/Startup.cs
+++ BRabbitMQ/BRabbitMQ.Banking.Api/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BRabbitMQ.Banking.Data.Context;
+using BRabbitMQ.Infrastructure.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BRabbitMQ.Banking.API
 {
@@ -26,6 +24,21 @@ namespace BRabbitMQ.Banking.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            RegisterServices(services);
+            services.AddMediatR(typeof(Startup));
+            services.AddDbContext<BankingDbContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
+                }
+                );
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Banking Microservice", Version = "v1"});
+            });
+        }
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +48,12 @@ namespace BRabbitMQ.Banking.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+            });
 
             app.UseHttpsRedirection();
 
