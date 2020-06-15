de8e50074ca15c1d86f35d571542c9b49ddbe7ec
Seshan Pillay <seshanpillay25@gmail.com>
2020-06-15 15:53:50 +0200

    Added BankingController and updated launchUrl's

---
+++ BRabbitMQ/BRabbitMQ.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BRabbitMQ.Banking.Application.Interfaces;
+using BRabbitMQ.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BRabbitMQ.Banking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : Controller
+    {
+        //Injecting account service
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        
+        //GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
